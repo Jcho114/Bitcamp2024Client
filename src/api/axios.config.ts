f33b74ad220a8a55dd2ed78248b5f71b97498369
Joseph Cho <108897459+Jcho114@users.noreply.github.com>
2024-04-21 07:27:05 -0400

    json

--- src/api/axios.config.ts
+++ src/api/axios.config.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: `/cors-proxy/`,
-  headers: { 'content-type': 'application/x-www-form-urlencoded' },
+  headers: { 'content-type': 'application/json' },
 });
 
 export default api;
