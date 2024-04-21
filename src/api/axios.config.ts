d81d25ea91c33d77caba6c9379f96004da11fb26
Joseph Cho <108897459+Jcho114@users.noreply.github.com>
2024-04-21 06:51:53 -0400

    plz

--- src/api/axios.config.ts
+++ src/api/axios.config.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
 
 const api = axios.create({
-  baseURL: "https://bitcamp2024server.onrender.com",
+  baseURL: "/cors-proxy/",
 });
 
 export default api;
